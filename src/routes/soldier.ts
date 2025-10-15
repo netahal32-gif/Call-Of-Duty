a3856041c4b0a78450f2992dafeccf32c39bb045
netahal32-gif <neta.hal32@gmail.com>
2025-10-15 16:50:13 +0300

    Add soldier schema and part of soldier route
    Add POST soldier without date
    
    Add dates to soldierOutput
    
    Change post soldier message
    
    Fix comments
    
    Fix new comments

--- src/routes/soldier.ts
+++ src/routes/soldier.ts
@@ -1,36 +1,27 @@
 import type { FastifyInstance } from "fastify";
-import { soldierSchema } from "./schemas/soldier.js";
-import type { Soldier } from "./schemas/soldier.js";
+import type { Soldier, SoldierOutput } from "./schemas/soldier.js";
+import { postSoldier } from "./helpers/soldierHelpers.js";
 
-interface SoldierOutput extends Soldier {
-    createdAt: Date;
-    updatedAt: Date;
-}
 
 const soldierRoutes = async (server: FastifyInstance) => {
-    server.post<{ Body: Soldier; Replay: SoldierOutput }>("/", async (req, res) => {
-        const collection = server.mongo.db?.collection("soldiers");
-        if (!collection) throw new Error("MongoDB collection not found");
-
-        const parseResult = soldierSchema.safeParse(req.body);
-
-        if (!parseResult.success) {
-            return res.status(400).send({
-                error: "Invalid request body",
-                details: parseResult.error.flatten(),
+    server.post<{ Body: Soldier; res: SoldierOutput }>("/", async (req, res) => {
+        try {
+            const soldier = await postSoldier(server, req.body);
+            res.status(201).send({ "Soldier added successfully": soldier });
+        } catch (err: any) {
+            server.log.error(err);
+            if (err.statusCode === 400) {
+                return res.status(400).send({
+                    error: err.message,
+                    details: err.errors,
+                });
+            }
+            res.status(500).send({
+                "Error Adding Soldier":
+                    err.message || err,
             });
         }
-
-        const soldier = parseResult.data;
-        
-        try {
-            const result = await collection.insertOne(soldier);
-            res.status(201).send({ "Soldier added successfully": soldier });
-        } catch (err) {
-            server.log.error(err);
-            res.status(500).send({ "Error Adding Soldier": err })
-        }
-    })
+    });
 };
 
 export default soldierRoutes;
