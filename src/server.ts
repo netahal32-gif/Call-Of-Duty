915198409f476c05db59b975081f01f79ac863fd
netahal32-gif <neta.hal32@gmail.com>
2025-10-16 11:32:15 +0300

    Fix comments

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastifyMongo from '@fastify/mongodb'
 import Fastify from 'fastify'
 import { loggerConfig } from './loggerConfig.js'
 import healthRoutes from './routes/health.js'
+import soldierRoutes from "./routes/soldier.js";
 
 const MONGO_URL = String(process.env.MONGO_URL)
 
@@ -12,8 +13,10 @@ const buildServer = async () => {
   await server.register(fastifyMongo, { forceClose: true, url: MONGO_URL })
 
   await server.register(healthRoutes, { prefix: '/health' })
+  await server.register(soldierRoutes, { prefix: "/soldiers" });
 
   return server
 }
+
 
 export default buildServer
