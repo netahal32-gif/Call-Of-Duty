a4c7d1e500e67781884e5acb22bdcbb55daa57e9
netahal32-gif <neta.hal32@gmail.com>
2025-10-26 14:19:00 +0200

    Finish Task2
    Fix some comments
    
    Fix comments
    
    Remover any type
    
    Change error logger
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Tempt
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix comments
    
    Fix lint
    
    Fix
    
    Fix
    
    TEMP
    
    TEMP
    
    Fix comments

--- tests/db-insert.ts
+++ tests/db-insert.ts
@@ -1,42 +1,9 @@
-import type { BaseDuty } from '../src/types/duty.js'
 import type { Soldier } from '../src/types/soldier.js'
-import {  defaultSoldierInput } from './data.js'
+import { buildSoldier } from './data.js'
 
-type SoldierPostBodyOptions = Soldier & {
-  blankRank?: boolean
-}
-
-export const defaultDutyInput: BaseDuty = {
-  constraints: ['No phones', 'Night duty'],
-  description: 'Soldiers will secure the main gate during night hours.',
-  endTime: new Date('2030-10-10T00:00:00Z'),
-  location: [34.7812, 32.0853],
-  maxRank: 3,
-  minRank: 1,
-  name: 'Guard the Main Gate',
-  soldiersRequired: 5,
-  startTime: new Date('2030-10-01T00:00:00Z'),
-  value: 100,
-}
-
-export const soldierPostBody = (input: Partial<SoldierPostBodyOptions> = {}) => {
-  const { _id, limitations, name, rank, blankRank } = { ...defaultSoldierInput, ...input }
-
+export const soldierPostBody = (input: Partial<Soldier> = {}) => {
+  const soldier = buildSoldier(input)
   return {
-    _id,
-    limitations,
-    name,
-    rank: blankRank
-      ? {}
-      : {
-        name: rank.name,
-        value: rank.value,
-      },
+    ...soldier,
   }
-}
-
-export const dutyPostBody = (input: Partial<BaseDuty> = {}) => {
-  const merged = { ...defaultDutyInput, ...input }
-
-  return { ...merged }
 }
