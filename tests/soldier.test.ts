5f8fadef78ec1d3a15d27ba07c6d61cb46fedd87
netahal32 <netahal32@gmail.com>
2025-11-20 10:54:59 +0200

    Finish Task3
    Finish Task4
    
    Finish Task4
    
    Finish Task4

--- tests/soldier.test.ts
+++ tests/soldier.test.ts
@@ -10,9 +10,6 @@ describe('Soldier Routes', () => {
   const pastDate = '2001-01-01'
 
   beforeAll(async () => {
-    const baseUrl = process.env.MONGO_URL!
-    const url = `${baseUrl}-soldier`
-    process.env.MONGO_URL = url
     server = await buildServer()
     soldierService = createSoldierService(server)
   })
@@ -197,7 +194,7 @@ describe('Soldier Routes', () => {
       const responseBody = response.json()
       expect(response.statusCode).toBe(500)
       expect(responseBody.message).toEqual(
-        `✖ E11000 duplicate key error collection: call-of-duty-test-soldier.soldiers index: _id_ dup key: { _id: "${_id}" }`,
+        `✖ E11000 duplicate key error collection: call-of-duty-test.soldiers index: _id_ dup key: { _id: "${_id}" }`,
       )
     })
   })
